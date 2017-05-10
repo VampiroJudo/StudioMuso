ba9515b57e05092cf93e85f3b32bbcfca1afd377
John <johnconley507@hotmail.com>
2017-05-10 15:19:16 -0400

    implementing jshint and jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var  gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
 var jsfiles = ['*.js', 'src.**/*.js'];
+
+
 gulp.task('style', function() {
-	gulp.src(jsfiles)
-		.pipe(jshint());
-})
+	return gulp.src(jsfiles)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish', {
+			verbose: true
+		}))
+		.pipe(jscs());
+});
